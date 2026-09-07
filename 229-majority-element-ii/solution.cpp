082ac17dff035c395b2c646349d697534a9a4fbf
arnavi <arnavissystems@gmail.com>
2026-09-07 23:47:19 +0530

    Majority Element II - 2026-09-07T18:17:18.232Z

---
+++ 229-majority-element-ii/solution.cpp
@@ -0,0 +1,17 @@
+// 5 ms | 26.5 MB
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for (int num : nums) {
+            mp[num]++;
+        }
+        for (auto it : mp) {
+            if (it.second > nums.size() / 3) {
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
